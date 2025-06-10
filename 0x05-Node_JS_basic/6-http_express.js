ccf4dd59f3b29d4bce5a1e75545cc75e14968bfa
Clinton431 <clintonnyakoe@gmail.com>
2025-06-10 10:21:18 +0300

    new

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,12 +1,14 @@
 const express = require('express');
-const app = express();
 
-app.get('/', (req, res) => {
+const app = express();
+const PORT = 1245;
+
+app.get('/', (_, res) => {
   res.send('Hello ALX!');
 });
 
-app.listen(1245, () => {
-  console.log('Server listening on PORT 1245');
+app.listen(PORT, () => {
+  console.log(`Server listening on PORT ${PORT}`);
 });
 
 module.exports = app;
